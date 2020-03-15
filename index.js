45c1f0b6d10630f84ecb50b9597214364bf5f81d
Daisy Zurita <daiszurita@gmail.com>
2020-03-15 22:12:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,17 +33,15 @@ function sayHiToGrandma(string){
     
     return 'I can\'t hear you!'
     
-var uppercase ='HELLO!'
+
      
   }
+  
+  var uppercase ='HELLO!'
   else if (uppercase.toUpperCase() === uppercase){
        
       return 'YES INDEED!'
   }
   
- 
-  
- 
-
 }
 
