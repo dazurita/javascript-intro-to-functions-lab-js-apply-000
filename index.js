a470aa889d80d5140871227f6c414551cad4ea4c
Daisy Zurita <daiszurita@gmail.com>
2020-03-15 22:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,15 +28,13 @@ function sayHiToGrandma(string){
     return 'I love you, too.'
   }
   
-  var lowercase = 'hello!'
-  var uppercase = 'HELLO!' 
    
   if (lowercase.toLowerCase() === lowercase){
     
     return 'I can\'t hear you!'
-  }
+
      
-  if (uppercase.toUpperCase() === uppercase){
+  else if (uppercase.toUpperCase() === uppercase){
        
       return 'YES INDEED!'
   }
