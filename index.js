bc7c86cb21f8fd2c1b7faf5168d0d3649d838140
Daisy Zurita <daiszurita@gmail.com>
2020-03-15 21:17:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function shout(string){
+  
+  return string.toUpperCase()
+  console.log(string)
+ 
+}
+
+function whisper(string){
+  
+  return string.toLowerCase()
+  console.log(string)
+}
+
+function logShout(string){
+
+  console.log(string.toUpperCase())
+  
+  
+}
+
+function logWhisper(string){
+  
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+
+  var uppercase = 'HELLO!'
+  var lowercase = 'hello!'
+  
+  if (uppercase.toUpperCase() === uppercase){
+  
+    
+    
+    return 'YES INDEED!'
+    
+  
+  if  (lowercase.toLowerCase() === lowercase){
+    return 'I can\'t hear you!'
+ if  (string === 'I love you, Grandma.'){
+    return 'I love you, too.'
+  }
+  return string
+}
+}}
